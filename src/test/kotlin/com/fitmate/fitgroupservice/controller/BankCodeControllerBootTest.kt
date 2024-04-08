18741e9e680ef09984af7bc9e4f91661ec038292
12OneTwo12 <jji042842@gmail.com>
2024-04-08 23:55:12 +0900

    application yml to on profile

--- src/test/kotlin/com/fitmate/fitgroupservice/controller/BankCodeControllerBootTest.kt
+++ src/test/kotlin/com/fitmate/fitgroupservice/controller/BankCodeControllerBootTest.kt
@@ -10,7 +10,6 @@ import org.springframework.http.MediaType
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*
 import org.springframework.restdocs.payload.PayloadDocumentation.*
-import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
@@ -19,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@TestPropertySource(locations = ["classpath:application-test.yml"])
 class BankCodeControllerBootTest {
 
     @Autowired
