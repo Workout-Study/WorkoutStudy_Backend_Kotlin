2a40eac92f6d71c60a317c9260f91eb123bc8cfe
12OneTwo12 <jji042842@gmail.com>
2024-06-23 14:57:37 +0900

    user id

--- src/main/kotlin/com/fitmate/fitgroupservice/dto/user/UserCreateMessageDto.kt
+++ src/main/kotlin/com/fitmate/fitgroupservice/dto/user/UserCreateMessageDto.kt
@@ -1,7 +1,7 @@
 package com.fitmate.fitgroupservice.dto.user
 
 data class UserCreateMessageDto(
-    val id: Int,
+    val userId: Int,
     val nickname: String,
     val state: Boolean,
     val createdAt: String,
