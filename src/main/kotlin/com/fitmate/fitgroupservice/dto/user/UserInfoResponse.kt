295dae314ec9f32a62b89a37afbc91be12978629
Jeong Jeongil <jji042842@gmail.com>
2024-08-12 12:14:35 +0900

    instant to formatted string

--- src/main/kotlin/com/fitmate/fitgroupservice/dto/user/UserInfoResponse.kt
+++ src/main/kotlin/com/fitmate/fitgroupservice/dto/user/UserInfoResponse.kt
@@ -1,11 +1,15 @@
 package com.fitmate.fitgroupservice.dto.user
 
+import com.fitmate.fitgroupservice.utils.DateParseUtils
 import java.time.Instant
 
 data class UserInfoResponse(
     val userId: Int,
     val nickname: String,
     val state: Boolean,
-    val createdAt: Instant,
-    val updatedAt: Instant
-)
+    private val createdAtInstant: Instant,
+    private val updatedAtInstant: Instant
+) {
+    val createdAt: String = DateParseUtils.instantToString(createdAtInstant)
+    val updatedAt: String = DateParseUtils.instantToString(updatedAtInstant)
+}
